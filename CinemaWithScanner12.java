1d8f33e4e074df28c5c86558e530ad1507114a8f
LevianVian <artalevi4@gmail.com>
2025-11-13 15:56:00 +0700

    Enhance CinemaWithScanner12: Improve audience input validation and display logic

--- CinemaWithScanner12.java
+++ CinemaWithScanner12.java
@@ -1,30 +1,76 @@
-
 import java.util.Scanner;
-
 
 public class CinemaWithScanner12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[][] audience = new String[4][2];
+        int choice;
 
-        int row, column;
-        String name, next;
+        while (true) {
+            System.out.println("\n===== Cinema Menu =====");
+            System.out.println("1. Input audience data");
+            System.out.println("2. Show audience list");
+            System.out.println("3. Exit");
+            System.out.print("Choose menu (1-3): ");
+            choice = sc.nextInt();
+            sc.nextLine();
 
-        while (true){
-            System.out.println("Enter a name: ");
-            name = sc.nextLine();
-            System.out.println("Enter row number: ");
-            row = sc.nextInt();
-            System.out.println("Enter column number: ");
-            column = sc.nextInt();
-            sc.nextLine(); 
+            if (choice == 1) {
+                while (true) {
+                    System.out.print("Enter name: ");
+                    String name = sc.nextLine();
 
-            audience[row - 1][column - 1] = name;
-            System.out.println("Are there any other audience? to be added? (y/n): ");
-            next = sc.nextLine();
+                    int row, column;
+                    while (true) {
+                        System.out.print("Enter row number (1-4): ");
+                        row = sc.nextInt();
+                        System.out.print("Enter column number (1-2): ");
+                        column = sc.nextInt();
+                        sc.nextLine(); 
 
-            if (next.equalsIgnoreCase("n")){
+                        if (row < 1 || row > 4 || column < 1 || column > 2) {
+                            System.out.println("X Seat not available. Try again X");
+                            continue;
+                        }
+
+                        if (audience[row - 1][column - 1] != null) {
+                            System.out.println("! Seat already occupied! Choose another seat !");
+                            continue;
+                        }
+
+                        audience[row - 1][column - 1] = name;
+                        break;
+                    }
+
+                    System.out.print("Add another audience? (y/n): ");
+                    String next = sc.nextLine();
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+            }
+
+            else if (choice == 2) {
+                System.out.println("\n===== Audience List =====");
+                for (int i = 0; i < audience.length; i++) {
+                    for (int j = 0; j < audience[i].length; j++) {
+                        if (audience[i][j] == null) {
+                            System.out.print("[***]\t");
+                        } else {
+                            System.out.print("[" + audience[i][j] + "]\t");
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+
+            else if (choice == 3) {
+                System.out.println("Program end");
                 break;
+            }
+
+            else {
+                System.out.println("X Invalid menu choice. Try again X");
             }
         }
         sc.close();
